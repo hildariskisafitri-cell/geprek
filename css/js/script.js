75887e317140ec6ecc12f5e304bf22925636e40d
hildariskisafitri-cell <hildariskisafitri@gmail.com>
2026-01-07 20:03:33 +0700

    update

--- css/js/script.js
+++ css/js/script.js
@@ -1,9 +1,19 @@
-// Tooggle class active
+// Tooggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger-menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
 };
+
+//toggel class active untuk search form
+const searchform = document.queryselector('.search-form');
+const searchbox = document.queryselector("#search-box");
+
+document.querySelector('#search-button').onclick = () => {
+  searchform.classlist.toggle('active');
+  searchbox.focus();
+  e.preventdefault();
+}
 
 // klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
